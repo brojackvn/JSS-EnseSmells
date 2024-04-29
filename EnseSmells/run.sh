71a76cad17e6370d220dfd8d7c2e2176088b9bf9
brojackvn <anhho.work@gmail.com>
2024-04-30 01:12:10 +0700

    edit github repository

--- EnseSmells/run.sh
+++ EnseSmells/run.sh
@@ -6,9 +6,9 @@ python ./program/ensesmells/main.py \
     --lr 0.025 \
     --threshold 0.5 \
     --hidden_size_lstm 100 \
-    --data_path "/content/drive/MyDrive/LabRISE/CodeSmellDetection/embedding-dataset/combine/GodClass/GodClass_TokenIndexing_metrics.pkl" \
-    --tracking_dir "/content/drive/MyDrive/LabRISE/DeepLearning-CodeSmell/DeepSmells/tracking/" \
-    --result_dir "/content/drive/MyDrive/LabRISE/DeepLearning-CodeSmell/DeepSmells/results/"
+    --data_path "/Users/brojackvn/Documents/my-work/JSS-EnseSmells/embedding-dataset/combine-Embedding&Metrics/GodClass/GodClass_TokenIndexing_metrics.pkl" \
+    --tracking_dir "/Users/brojackvn/Documents/my-work/JSS-EnseSmells/EnseSmells/tracking" \
+    --result_dir "/Users/brojackvn/Documents/my-work/JSS-EnseSmells/EnseSmells/results"
 
 # Here is the configure of the model
 # Note: Set the configuration
